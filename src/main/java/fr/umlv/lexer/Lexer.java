225cc85bedbf4be98b6dcf90d32f6af5cad66c88
Romain Warnan <romain.warnan@gmail.com>
2019-04-19 09:44:08 +0200

    Q1

--- src/main/java/fr/umlv/lexer/Lexer.java
+++ src/main/java/fr/umlv/lexer/Lexer.java
@@ -1,3 +1,24 @@
 package fr.umlv.lexer;
 
-// TODO
+import java.util.Objects;
+import java.util.Optional;
+
+public class Lexer<T> {
+
+    private static Lexer instance;
+
+    private Lexer(){
+    }
+
+    public static <T> Lexer<T> create() {
+        if(instance == null) {
+            instance = new Lexer<>();
+        }
+        return instance;
+    }
+
+    public Optional<T> tryParse(String token) {
+        Objects.requireNonNull(token);
+        return Optional.empty();
+    }
+}
