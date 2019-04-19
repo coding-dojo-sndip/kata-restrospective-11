f1f5d31eab1d3662c61ce8b70b8f4a5f08d00ea2
Romain Warnan <romain.warnan@gmail.com>
2019-04-19 16:01:22 +0200

    Q2

--- src/main/java/fr/umlv/lexer/Lexer.java
+++ src/main/java/fr/umlv/lexer/Lexer.java
@@ -2,23 +2,44 @@ package fr.umlv.lexer;
 
 import java.util.Objects;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lexer<T> {
 
-    private static Lexer instance;
+    private static final Lexer SIMPLE_LEXER = new Lexer<>();
+    private static final Pattern ONLY_ONE_CAPTURE_GROUP = Pattern.compile("[^()]*\\([^()]+\\)[^()]*");
+    private Pattern pattern;
 
-    private Lexer(){
+    private Lexer(){ }
+
+    private Lexer(Pattern pattern){
+        this.pattern = pattern;
     }
 
-    public static <T> Lexer<T> create() {
-        if(instance == null) {
-            instance = new Lexer<>();
-        }
-        return instance;
+    public static <T> Lexer create() {
+        return SIMPLE_LEXER;
     }
 
-    public Optional<T> tryParse(String token) {
+    public static <T> Lexer<T> from(Pattern pattern) {
+        Objects.requireNonNull(pattern);
+        Matcher matcher = ONLY_ONE_CAPTURE_GROUP.matcher(pattern.pattern());
+        if(matcher.matches()) return new Lexer<>(pattern);
+        throw new IllegalArgumentException();
+    }
+
+    public static <T> Lexer<T> from(String regex) {
+        Objects.requireNonNull(regex);
+        return Lexer.from(Pattern.compile(regex));
+    }
+
+    public Optional<String> tryParse(String token) {
         Objects.requireNonNull(token);
+        if(pattern == null) return Optional.empty();
+        Matcher matcher = pattern.matcher(token);
+        if(matcher.matches() && matcher.groupCount() == 1) {
+            return Optional.of(matcher.group(1));
+        }
         return Optional.empty();
     }
 }
