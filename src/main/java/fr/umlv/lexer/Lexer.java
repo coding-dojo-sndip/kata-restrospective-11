5022dc3073cfb3a8582e9366840d76370df8d1e6
Romain Warnan <romain.warnan@gmail.com>
2019-04-22 16:18:04 +0200

    Q1

--- src/main/java/fr/umlv/lexer/Lexer.java
+++ src/main/java/fr/umlv/lexer/Lexer.java
@@ -1,3 +1,17 @@
 package fr.umlv.lexer;
 
-// TODO
+import java.util.Objects;
+import java.util.Optional;
+
+@FunctionalInterface
+interface Lexer<T> {
+
+    Optional<T> tryParse(String input);
+
+    static <T> Lexer<T> create() {
+        return (String s) -> {
+            Objects.requireNonNull(s);
+            return Optional.empty();
+        };
+    }
+}
